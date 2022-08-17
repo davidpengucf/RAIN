df61454366e291a9604c97f082539f53081fd82b
davidpengucf <102485354+davidpengucf@users.noreply.github.com>
2022-08-16 21:10:13 -0400

    Update train_source.py

--- train_source.py
+++ train_source.py
@@ -424,14 +424,6 @@ import pdb
 
 
 class CrossEntropyLabelSmooth(nn.Module):
-    """Cross entropy loss with label smoothing regularizer.
-    Reference:
-    Szegedy et al. Rethinking the Inception Architecture for Computer Vision. CVPR 2016.
-    Equation: y = (1 - epsilon) * y + epsilon / K.
-    Args:
-        num_classes (int): number of classes.
-        epsilon (float): weight.
-    """
 
     def __init__(self, num_classes, epsilon=0.1, use_gpu=False, reduction=True):
         super(CrossEntropyLabelSmooth, self).__init__()
